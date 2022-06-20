12163dc8c24d62984f4752ebe426479d2178b389
simonasablinaite <simonasablinaite@gmail.com>
2022-06-20 09:19:00 +0300

    js: class random comments

--- js/class-random.js
+++ js/class-random.js
@@ -12,21 +12,28 @@ class PageHome {
   }
 }
 
+// Susiejimas ka vartotojas gali parasyti ir ka mes tada darytume (kuria klase kviesti)
 const router = {
   '': PageHome,
   '404': Page404,
 }
 
+// vartotojo ivestas kelias
 const trimmedPath = 'as'
 
+// reikiamos naudoti klases atranka, kai 404 yra musu default pasirinkimas
 let pageClass = router['404'];
 if (trimmedPath in router) {
   pageClass = router[trimmedPath];
 }
 
+// pagal rasta naudojimui klase, kuriamas jos objektas
 const pageObj = new pageClass();
+
+// ir tas objektas generuojasavo HTML turini
 const HTML = pageObj.render();
 
+// spausdinama...
 console.log(pageClass);
 console.log(pageObj);
 console.log(HTML);
